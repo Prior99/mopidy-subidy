fe444091a6677808dd0037086e90140542602097
hhm <heehooman+vcs-ci@gmail.com>
2017-03-27 15:49:18 -0400

    B"H playlist get items fix method call

--- mopidy_subidy/playlists.py
+++ mopidy_subidy/playlists.py
@@ -22,7 +22,7 @@ class SubidyPlaylistsProvider(backend.PlaylistsProvider):
 
     def get_items(self, items_uri):
         #logger.info('ITEMS %s: %s' % (lookup_uri, self.subsonic_api.get_playlist_songs_as_refs(uri.get_playlist_id(items_uri))))
-        return self.subsonic_api.get_playlist_songs_as_refs(uri.get_playlist_id(items_uri))
+        return self.subsonic_api.get_playlist_as_songs_as_refs(uri.get_playlist_id(items_uri))
 
     def lookup(self, lookup_uri):
         #logger.info('LOOKUP PLAYLIST %s: %s' % (lookup_uri, self.subsonic_api.get_playlist_as_playlist(uri.get_playlist_id(lookup_uri))))
